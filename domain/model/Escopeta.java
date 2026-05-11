f2744e54992ce8d85605a4608ee6c6ce76e16406
jamesvv27 <jaimesantiagoalatorre1b@gmail.com>
2026-05-11 17:36:56 -0600

    Agregar clase Escopeta

---
+++ domain/model/Escopeta.java
@@ -0,0 +1,95 @@
+package domain.model;
+
+import domain.enums.Disparo;
+import domain.util.interfaces.ModoAutomatico;
+
+public class Escopeta extends Arma implements ModoAutomatico{
+    
+    private int numeroDisparos;
+
+    public Escopeta(String nombreArma, int cargadorActual, int dimensionCargador, int municionReserva,
+            int porcentajePrecision, int cadenciaDisparo, Disparo modoDisparo,
+            int numeroDisparos){
+            super(nombreArma, cargadorActual, dimensionCargador, municionReserva, porcentajePrecision, cadenciaDisparo, Disparo.SOLO);
+            this.numeroDisparos = 1;
+    }
+
+    @Override
+    public void disparar(){
+        int n_Balas = calcularDisparosPosibles();
+        if(puedeDisparar()){
+            setCargadorActual(getCargadorActual() - n_Balas);
+        }
+         System.out.print(getNombreArma() + "dispara " + n_Balas + " balas en " + obtenerModoDisparo());
+    }
+
+    @Override
+    public int cambiarModoDisparo(){
+
+        if(esModoEsperado(Disparo.SOLO, "Cambiar por AUTOMATICO")){
+            cambiarPorAutomatico();
+            return 1;
+        }
+
+        if(esModoEsperado(Disparo.AUTOMATICO, "Cambiar por SOLO")){
+            cambiarPorSolo();
+            return 2;
+        }
+
+        return 0;
+    }
+
+    @Override 
+    public String obtenerModoDisparo(){
+
+        if(esModoEsperado(Disparo.SOLO, "Mostrar SOLO")){
+            return "DISPARO UNICO";
+        }
+
+        if(esModoEsperado(Disparo.AUTOMATICO, "Mostrar AUTOMATICO")){
+            return "RAFAGA";
+        }
+
+        return "DISPARO " + getModoDisparo();
+
+    }
+
+    public int getNumeroDisparos(){
+        return numeroDisparos;
+    }
+
+    private int calcularDisparosPosibles(){
+        return getCargadorActual() < this.numeroDisparos ? getCargadorActual() : this.numeroDisparos;
+    }
+
+    private void cambiarPorAutomatico(){
+        setModoDisparo(Disparo.AUTOMATICO);
+        alternarRafaga();
+    }
+
+    private void cambiarPorSolo(){
+        setModoDisparo(Disparo.SOLO);
+        alternarUnico();
+    }
+
+    private void alternarUnico(){
+        this.numeroDisparos = 1;
+    }
+
+    private void alternarRafaga(){
+        this.numeroDisparos = 6;
+    }
+
+
+    private boolean esModoEsperado(Disparo esperado, String accion){
+        if(getModoDisparo() != esperado){
+            System.out.print("No se puede " + accion + " cuando modoDisparo =" +
+            getModoDisparo()
+            );
+            return false;
+        }
+        return true;
+    }
+
+
+}
