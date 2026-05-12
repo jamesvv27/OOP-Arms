8dad2de3226d0be69c91da9ebe5332c802fdbb12
jamesvv27 <jaimesantiagoalatorre1b@gmail.com>
2026-05-11 20:31:05 -0600

    Agregar clase hija de Pistola

---
+++ domain/model/DesertEagle.java
@@ -0,0 +1,68 @@
+package domain.model;
+
+import domain.enums.*;
+import domain.util.interfaces.Personalizable;
+
+public class DesertEagle extends Pistola implements Personalizable{
+
+    // Alternar entre Variante Default/(Accesorio.NINGUNO) y Accesorio.CARGADOR_EXPANDIDO
+
+    private Accesorio accesorioActual; // Una sola ranura de accesorios
+
+    public DesertEagle(String nombreArma, int cargadorActual, int dimensionCargador, int municionReserva,
+            int porcentajePrecision, int cadenciaDisparo, Disparo modoDisparo,
+            Accesorio accesorioActual) {
+        super("Desert Eagle", 0, 8,
+            16, 70,
+            2,
+                Disparo.SOLO);
+                this.accesorioActual = Accesorio.NINGUNO;
+    }
+
+    public Accesorio getAccesorioActual(){
+        return accesorioActual;
+    }
+
+    /*
+        Comparar el accesorio deseado ciclando en agregarAccesorio(); verificar si 
+        el Accesorio deseado es igual a cada uno de los accesorios disponibles
+
+        Verificar si el accesorio a agregar es distinto al actual en estaAccesorioOcupado()
+        --> Asegurarse de que CARGADOR_EXPANDIDO no se encuentre activado.
+
+        Si se va a agregar un accesorio, solo podra ser CARGADOR_EXPANDIDO
+     */
+
+    @Override
+    public int agregarAccesorio(Accesorio deseado) {
+        if(esMismoAccesorio(deseado, Accesorio.CARGADOR_EXPANDIDO)){
+            agregarAccesorioMag();
+            return 1;
+        }
+        return 0;
+    }
+
+    // Unico accesorio disponible para Desert Eagle
+    private void agregarAccesorioMag(){ // Mag => CARGADOR_EXPANDIDO
+        if(!estaAccesorioOcupado(Accesorio.CARGADOR_EXPANDIDO, "Agregar Cargador Ext"))
+            setAccesorio(Accesorio.CARGADOR_EXPANDIDO);
+    }
+
+    private void setAccesorio(Accesorio nuevoAccesorio){
+        this.accesorioActual = nuevoAccesorio;
+    }
+
+    private boolean esMismoAccesorio(Accesorio deseado, Accesorio solicitado){
+        return deseado == solicitado;
+    }
+
+    private boolean estaAccesorioOcupado(Accesorio esperado, String accion){
+        if(getAccesorioActual() == esperado){
+            System.out.print("No se puede " + accion + " cuando accesorioActual = " +
+                getAccesorioActual() + ". Esperado = " + esperado
+            );
+            return true;
+        }
+        return false;
+    }
+}
