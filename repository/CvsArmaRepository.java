3c359f6e67a09547311547de1d3d308210dad67f
jamesvv27 <jaimesantiagoalatorre1b@gmail.com>
2026-05-30 19:06:37 -0600

    Formato en CvsArmaRepo

--- repository/CvsArmaRepository.java
+++ repository/CvsArmaRepository.java
@@ -15,8 +15,7 @@ public class CvsArmaRepository implements ArmaRepository  {
         this.ruta = ruta;
     }
 
-
-    @Override // Reparar ruta de ArchivoPersonajeException
+    @Override // Reparada ruta de ArchivoPersonajeException
     public List<Arma> getListaArma() throws ArchivoArmaException {
         
         final List<Arma> armas = new ArrayList<>();
@@ -24,11 +23,14 @@ public class CvsArmaRepository implements ArmaRepository  {
         try {
             final BufferedReader reader = new BufferedReader(new FileReader(ruta));
             String linea;
-            reader.readLine();//opcional dependiendo el caso
+            reader.readLine();
 
             while ((linea = reader.readLine()) != null) {
                 final String[] columnas = linea.split(",");
-                final ArmaData data = new ArmaData(columnas[0], columnas[1], Integer.parseInt(columnas[2]), Integer.parseInt(columnas[3]), Integer.parseInt(columnas[4]), Integer.parseInt(columnas[5]), Integer.parseInt(columnas[6]));
+                final ArmaData data = new ArmaData(columnas[0], columnas[1],
+                    Integer.parseInt(columnas[2]), Integer.parseInt(columnas[3]),
+                    Integer.parseInt(columnas[4]), Integer.parseInt(columnas[5]),
+                    Integer.parseInt(columnas[6]));
                 final Arma arma = ArmaFactory.crearDesdeData(data);
                 armas.add(arma);
             }
@@ -37,10 +39,7 @@ public class CvsArmaRepository implements ArmaRepository  {
             throw new ArchivoArmaException("No se pudieron cargar las armas desde CSV", e);
         }
         return armas;
-
         
     }
-
-    
 
 }
